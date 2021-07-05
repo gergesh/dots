60a65c88c448375296580cf8509270f45fb9aba9
Yoav Shai <gergesh@gmail.com>
2021-07-06 01:00:47 +0300

    Add neogit plugin

--- init.lua
+++ init.lua
@@ -28,6 +28,11 @@ require('packer').startup(function()
   use {'lewis6991/gitsigns.nvim', requires = {'nvim-lua/plenary.nvim'} }
   use 'neovim/nvim-lspconfig'        -- Collection of configurations for built-in LSP client
   use 'hrsh7th/nvim-compe'           -- Autocompletion plugin
+  -- Git integration
+  use {
+    'TimUntersberger/neogit', requires = { 'nvim-lua/plenary.nvim' },
+    config = function() require('neogit').setup() end
+  }
 end)
 
 --Incremental live completion
