6538e8031bf33330802119f32afca3b52dd604b3
Yoav Shai <gergesh@gmail.com>
2021-07-07 00:30:03 +0300

    Configure gitsigns plugin

--- init.lua
+++ init.lua
@@ -25,7 +25,10 @@ require('packer').startup(function()
   -- Add indentation guides even on blank lines
   use { 'lukas-reineke/indent-blankline.nvim' }
   -- Add git related info in the signs columns and popups
-  use {'lewis6991/gitsigns.nvim', requires = {'nvim-lua/plenary.nvim'} }
+  use {
+    'lewis6991/gitsigns.nvim', requires = {'nvim-lua/plenary.nvim'},
+    config = function() require('gitsigns').setup() end
+  }
   use 'neovim/nvim-lspconfig'        -- Collection of configurations for built-in LSP client
   use 'kabouzeid/nvim-lspinstall'    -- Add LspInstall command
   use 'hrsh7th/nvim-compe'           -- Autocompletion plugin
